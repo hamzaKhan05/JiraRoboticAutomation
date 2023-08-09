9c8435987f9053c070f8829f8c63478a31a0f554
yasinjodat <yasinjodat1390@gmail.com>
2023-08-09 15:42:34 +0500

    Changes made to the python file

--- RenameDuplicateColumns.py
+++ RenameDuplicateColumns.py
@@ -1,9 +1,24 @@
-
-def duplicates(In_FileNameArgument):
+def configure(FileName):
     import pandas as pd
-    df=pd.read_csv(In_FileNameArgument)
-    df.to_csv(In_FileNameArgument,index=False)
-    return
-
-
-
+    df=pd.read_csv(f"{FileName}.csv")
+    return df.to_json(orient="records")
+def filter():
+    import pandas as pd
+    df_jira=pd.read_csv("JiraToPythonDt.csv")
+    configure=pd.read_excel("Configuration.xlsx",sheet_name='Sheet1')
+    status=[]
+    row=[]
+    columns=[]
+    for i in configure['Columns']:
+        if isinstance(i,str):
+            columns.append(i)
+    for i in configure['Status']:
+        if isinstance(i,str):
+            status.append(i)
+    for i in status:
+        temp=df_jira[df_jira['Status']==f'{i}']
+        for j in temp.index:
+            row.append(df_jira.iloc[j])
+    df_new=pd.DataFrame(row)
+    df_filtered=df_new[columns] 
+    return df_filtered.to_json(orient="records")
